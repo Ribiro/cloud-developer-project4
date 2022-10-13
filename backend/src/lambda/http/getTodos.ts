ccb6b0fcb0ebbdfaf9e8382a640236fa206fe83c
Ribiro <deno059861>
2022-10-13 20:14:10 +0300

    final commit

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,9 +1,11 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
+import * as middy from 'middy';
 
 import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda';
 import {getAllToDo} from "../../businessLogic/ToDo";
+import { cors } from 'middy/middlewares'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const getTodosHandler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // TODO: Get all TODO items for a current user
     console.log("Processing Event ", event);
     const authorization = event.headers.Authorization;
@@ -15,11 +17,18 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     return {
         statusCode: 200,
         headers: {
+            'Access-Control-Allow-Headers': 'Content-Type',
             "Access-Control-Allow-Origin": "*",
-            'Access-Control-Allow-Credentials': true
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
         body: JSON.stringify({
             "items": toDos,
+            // "items": {
+            //     "test": "true"
+            // }
         }),
     }
 };
+
+export const handler = middy(getTodosHandler).use(cors({ credentials: true }));
--- client/src/config.ts
+++ client/src/config.ts
@@ -6,6 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-ribiro.us.auth0.com',            // Auth0 domain
-  clientId: 'miaQX8kiaYRamBanKsmTjS6uvDj3PfrO',          // Auth0 client id
+  clientId: 'yVVTt1Zk4QhH154PEthmzGWZlRLavTse',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
